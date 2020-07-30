a2a79f93f1646781cd428d9c77859df29d4220f8
Instinktaz <tomas.paskovskij@gmail.com>
2020-07-30 13:35:09 +0300

    Updates

---
+++ precache-manifest.34e7c4a62f4f75f5c2e2ecfb52b9e6ab.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4194ca2da0358bc8093d1c3f59230f",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "141ec1282a039363952c",
+    "url": "/starwars/static/css/2.d83ecddd.chunk.css"
+  },
+  {
+    "revision": "ecab7119397ec28ab2e9",
+    "url": "/starwars/static/css/main.3e98dbdf.chunk.css"
+  },
+  {
+    "revision": "141ec1282a039363952c",
+    "url": "/starwars/static/js/2.64803634.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/starwars/static/js/2.64803634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecab7119397ec28ab2e9",
+    "url": "/starwars/static/js/main.b6aaf514.chunk.js"
+  },
+  {
+    "revision": "1f4c9aac975eb98c99e4",
+    "url": "/starwars/static/js/runtime-main.8af6987b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/starwars/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/starwars/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/starwars/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/starwars/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/starwars/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
